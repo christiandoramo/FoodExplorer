4e5eec12b27ce525b514e5745187b3c5db8e08c2
christiandoramo <christiandoramo@gmail.com>
2024-01-25 12:06:24 -0300

    inicio do projeto

---
+++ api food explorer/src/server.ts
@@ -0,0 +1,26 @@
+require('express-async-errors')
+require('dotenv/config')
+const cors = require('cors')
+const express = require('express')
+import { NextFunction, Request, Response } from "express"
+import AppError from "./utils/AppError"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    console.error(error)
+    if (error instanceof AppError)
+        return response.status(error.status).json({
+            status: error.status,
+            message: error.message
+        })
+    return response.status(500).json({
+        status: 500,
+        message: 'Internal Server Error'
+    })
+})
+
+const SERVER_PORT = process.env.SERVER_PORT || 3000
+app.listen(SERVER_PORT, () => console.log('Server is running on PORT', SERVER_PORT))
---
+++ api food explorer/src/utils/AppError.ts
@@ -0,0 +1,9 @@
+class AppError{
+    message
+    status
+    constructor(message:string, status:number) {
+        this.message = message
+        this.status = status
+    }
+}
+export default AppError;
